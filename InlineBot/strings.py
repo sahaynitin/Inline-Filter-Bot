04fcc2a0298a8ebc96cd378c78ffd600ae2b31bf
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-09-19 10:31:44 +0530

    Update strings.py

--- InlineBot/strings.py
+++ InlineBot/strings.py
@@ -15,7 +15,7 @@ if CUSTOM_START_MESSAGE:
 else:
     START_MESSAGE = """<b>Hello {mention},
 
-I am an Inline Saver Bot, you can save inline filters and It can be use in any of your chats easily, Click help for more details</b> 
+I am an Inline Filter bot , you can save inline filters and It can be use in any of your chats easily, Click help for more details</b> 
 """
 
 HELP_MESSAGE = f"""<b><u>Main Available Commands</u></b>
@@ -50,9 +50,9 @@ HELP_MESSAGE = f"""<b><u>Main Available Commands</u></b>
 ABOUT_MESSAGE = f"""<b><u>ABOUT ME</u></b>
 
 <b>○ Maintained by : <a href='tg://user?id={OWNER_ID}'>This Person</a>
-○ Channel : <a href='https://t.me/CodeXBotz'>Code 𝕏 Botz</a>
-○ Support : <a href='https://t.me/CodeXBotzSupport'>Code 𝕏 Botz Support</a>
-○ Source Code : <a href='https://github.com/CodeXBotz/Inline-Filter-Bot'>Click here</a>
+○ Channel : <a href='https://t.me/Tellybots_4u'>Code Tellybots</a>
+○ Support : <a href='https://t.me/tellybots_support'>Code Tellybots Support</a>
+○ Version : <a href='https://t.me/Tellybots_digital'>Version 2.5</a>
 ○ Language : <a href='https://www.python.org/'>Python 3</a>
 ○ Library : <a href='https://github.com/pyrogram/pyrogram'>Pyrogram Asyncio {__version__}</a></b>
 """
@@ -80,16 +80,16 @@ MARKDOWN_HELP = """<b><u>Markdown Formatting</u></b>
     show as: <s>StrikeThrough Text</s>
     
 ○ <b>Hyper Link</b>
-    format: <code>[Text](https://t.me/CodeXBotz)</code>
-    show as: <a href='https://t.me/CodeXBotz'>Text</a>
+    format: <code>[Text](https://t.me/Tellybots_4u)</code>
+    show as: <a href='https://t.me/tellybots_4u'>Text</a>
     
 ○ <b>Buttons</b>
     <u>Url Button</u>:
-    <code>[Button Text](buttonurl:https://t.me/CoddeXBotz)</code>
+    <code>[Button Text](buttonurl:https://t.me/tellybots_4u)</code>
     <u>Alert Button</u>:
     <code>[Button Text](buttonalert:Alert Text)</code>
     <u>In Sameline</u>:
-    <code>[Button Text](buttonurl:https://t.me/CodeXBotz:same)</code></i>
+    <code>[Button Text](buttonurl:https://t.me/tellybots_4u:same)</code></i>
 
 ○ <b>Notes:</b>
     <i>Keep every Buttons in Seperate line when formating</i>
